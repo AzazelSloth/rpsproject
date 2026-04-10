ede3c32f1cfbac2096360cac4f226bd206d7f284
AzazelSloth <kajutokirigaya4@gmail.com>
2026-04-10 12:22:21 +0300

    refactor(main): improve port handling in bootstrap function

--- rps-backend/src/main.ts
+++ rps-backend/src/main.ts
@@ -59,6 +59,7 @@ async function bootstrap() {
     });
   }
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
