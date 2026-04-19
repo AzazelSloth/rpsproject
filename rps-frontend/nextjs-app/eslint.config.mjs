0142eea5f35f642a2610aef0788cdf8c01e2cac3
AzazelSloth <kajutokirigaya4@gmail.com>
2026-04-19 20:43:04 +0300

    fix(deployment): correct env variables interpolation for manual deployment

--- rps-frontend/nextjs-app/eslint.config.mjs
+++ rps-frontend/nextjs-app/eslint.config.mjs
@@ -1,18 +1,26 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig } from "eslint/config";
+import next from "@next/eslint-plugin-next";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  {
+    plugins: {
+      "@next/next": next,
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+      ...next.configs["core-web-vitals"].rules,
+    },
+  },
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      ".git/**",
+      "node_modules/**",
+    ],
+  },
 ]);
 
 export default eslintConfig;
